32a74bb49be8d813938b4ec09c918a150101c84c
kenya-sk <kenya0sakka@gmail.com>
2018-12-18 18:08:20 +0900

    Issue29: add lstm

--- src/python/lstm_pred.py
+++ src/python/lstm_pred.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 #coding: utf-8
 
-import matplotlib.pyplot as plt
+import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
