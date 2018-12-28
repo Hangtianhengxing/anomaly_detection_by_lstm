8cdd2acae6ce00f53995f29f76afc9a158f6e8e9
kenya-sk <kenya0sakka@gmail.com>
2018-12-28 15:05:27 +0900

    dev: add scaler

--- src/model/eval_loss.py
+++ src/model/eval_loss.py
@@ -2,26 +2,42 @@
 #coding: utf-8
 
 import numpy as np
+import pandas as pd
+
+from lstm_util import scale
 
 
-def pred_loss(pred_path, ans_path, invalid_time=2400):
+def pred_loss(train_path, ans_path, pred_path, invalid_time=2400):
+    train_df = pd.read_csv(train_path)
+    y_train = train_df["label"].as_matrix()
+    X_train = train_df.as_matrix()
+
+    ans_df = pd.read_csv(ans_path)
+    y_ans = ans_df["label"].as_matrix()
+    X_ans = ans_df.as_matrix()
+    
+    # applied MinMaxScaler
+    _, _, _, y_ans, _, _ = scale(X_train, X_ans, y_train, y_ans)
+
+    # pred date is already scaled
     pred_arr = np.loadtxt(pred_path)
-    ans_arr = np.loadtxt(ans_path)
 
     # remove invalid time
-    data_num = min(len(pred_arr), len(ans_arr))
+    data_num = min(len(pred_arr), len(y_ans))
     pred_arr = pred_arr[invalid_time:data_num]
-    ans_arr = ans_arr[invalid_time:data_num]
-    assert len(pred_arr) == len(ans_arr)
+    y_ans = y_ans[invalid_time:data_num]
+    assert len(pred_arr) == len(y_ans)
 
     # Mean Squared Error
-    loss = np.sum(np.abs(pred_arr - ans_arr)**2)
+    loss = np.sum(np.abs(pred_arr - y_ans)**2)
 
     print("Loss is {0}".format(loss))
 
 
 if __name__ == "__main__":
+    # train data using to scale dataset
+    train_path = "/Users/sakka/cnn_anomaly_detection/data/datasets/datasets/train.csv"
+    ans_path = "/Users/sakka/cnn_anomaly_detection/data/datasets/datasets/test.csv"
     pred_path = "/Users/sakka/cnn_anomaly_detection/data/prediction/default/value_20181220_1207.csv"
-    ans_path = "/Users/sakka/cnn_anomaly_detection/data/prediction/test_label.csv"
     invalid_time = 2400
-    pred_loss(pred_path, ans_path, invalid_time)
+    pred_loss(train_path, ans_path, pred_path, invalid_time)
