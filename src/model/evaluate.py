099d7217685fdf80ab5ec6713343451301ea4db8
kenya-sk <kenya0sakka@gmail.com>
2019-01-16 21:46:36 +0900

    dev: modify

--- src/model/evaluate.py
+++ src/model/evaluate.py
@@ -7,24 +7,24 @@ import pandas as pd
 import logging
 
 
-def eval_metrics(tp_cnt, fp_cnt, fn_cnt):
-    if (tp_cnt+fp_cnt+fn_cnt) != 0:
-        accuracy = (tp_cnt)/(tp_cnt+fp_cnt+fn_cnt)
+def eval_metrics(tp_cnt, fp_cnt, tn_cnt, fn_cnt):
+    if (tp_cnt + tn_cnt + fp_cnt + fn_cnt) != 0:
+        accuracy = (tp_cnt + tn_cnt) / (tp_cnt + tn_cnt + fp_cnt + fn_cnt)
     else:
         accuracy = 0
 
-    if (tp_cnt+fp_cnt) != 0:
-        precision = (tp_cnt)/(tp_cnt+fp_cnt)
+    if (tp_cnt + fp_cnt) != 0:
+        precision = (tp_cnt) / (tp_cnt + fp_cnt)
     else:
         precision = 0
 
-    if (tp_cnt+fn_cnt) != 0:
-        recall = (tp_cnt)/(tp_cnt+fn_cnt)
+    if (tp_cnt + fn_cnt) != 0:
+        recall = (tp_cnt) / (tp_cnt + fn_cnt)
     else:
         recall = 0
 
-    if (recall+precision) != 0:
-        f_measure = (2*precision*recall)/(recall+precision)
+    if (recall + precision) != 0:
+        f_measure = (2 * precision * recall) / (recall + precision)
     else:
         f_measure = 0
 
@@ -37,33 +37,34 @@ def eval_metrics(tp_cnt, fp_cnt, fn_cnt):
 
 
 def evaluate(pred_arr, bin_ans_arr, pred_point, invalid_time, thresh):
-    assert len(pred_arr) == (len(bin_ans_arr) + pred_point)
+    assert len(pred_arr) == (len(bin_ans_arr))
     valid_pred_arr = pred_arr.copy()[invalid_time:]
     valid_bin_arr = bin_ans_arr.copy()[invalid_time:]
 
     pred_idx = np.where(valid_pred_arr > thresh)[0]
-    tp_cnt, fp_cnt = 0, 0
-    for start_idx in pred_idx:
-        end_idx = min(len(pred_arr), start_idx+pred_point)
-        if np.sum(valid_bin_arr[start_idx:end_idx]) > 0:
-            tp_cnt += 1
-        else:
-            fp_cnt += 1
+    logger.debug("Number of alert: {}".format(len(pred_idx)))
 
-    ans_idx = np.where(valid_bin_arr == 1)[0]
+    tp_cnt = 0
+    fp_cnt = 0
+    tn_cnt = 0
     fn_cnt = 0
-    for end_idx in ans_idx:
-        start_idx = max(0, end_idx-pred_point)
-        detect = np.where((start_idx <= pred_idx) & (pred_idx <= end_idx))[0]
-        if len(detect) == 0:
+    for i in range(len(valid_pred_arr)):
+        if (valid_pred_arr[i] >= thresh) & (sum(valid_bin_arr[i:i + pred_point]) > 0):
+            tp_cnt += 1
+        elif (valid_pred_arr[i] >= thresh) & (sum(valid_bin_arr[i:i + pred_point]) == 0):
+            fp_cnt += 1
+        elif (valid_pred_arr[i] <= thresh) & (sum(valid_bin_arr[i:i + pred_point]) == 0):
+            tn_cnt += 1
+        elif (valid_pred_arr[i] <= thresh) & (sum(valid_bin_arr[i:i + pred_point]) > 0):
             fn_cnt += 1
 
     logger.debug("TP : {0}".format(tp_cnt))
     logger.debug("FP : {0}".format(fp_cnt))
+    logger.debug("TN : {0}".format(tn_cnt))
     logger.debug("FN : {0}".format(fn_cnt))
 
     accuracy, precision, recall, f_measure = eval_metrics(
-        tp_cnt, fp_cnt, fn_cnt)
+        tp_cnt, fp_cnt, tn_cnt, fn_cnt)
 
     return accuracy, precision, recall, f_measure
 
@@ -85,6 +86,8 @@ if __name__ == "__main__":
     ans_path = "../../data/datasets/test_label.csv"
     logger.debug("Answer data path: \"{0}\"".format(ans_path))
     bin_arr = np.loadtxt(ans_path)
+
+    pred_arr = pred_arr[:len(bin_arr)]
 
     # evaluate
     n_prev = 60*30
